d2e46dd865d04d45a302f89bd65f469f79978f2e
erutkovskis <72525572+erutkovskis@users.noreply.github.com>
2024-01-25 10:47:13 +0000

    Checkpoint 1. Initial commit

---
+++ test_stopper.ino
@@ -0,0 +1,26 @@
+int optoSw = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(optoSw,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue1 = analogRead(A0);
+  int sensorValue2 = analogRead(A7);
+  float voltage1 = sensorValue1 * (5.0 / 1023.0);
+  float voltage2 = sensorValue2 * (5.0 / 1023.0);
+  float voltageDif = voltage2 - voltage1;
+  Serial.println(voltageDif);
+  delay(1);
+  // if (voltageDif < 0.1) {
+  //   digitalWrite(optoSw,LOW);
+  //   Serial.println("0");
+  // }
+  // else {
+  //   digitalWrite(optoSw,HIGH);
+  //   Serial.println("1");
+  // }
+}
